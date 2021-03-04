e1baee245217ceac08e132c27009a49a081b3f0d
universuen <universuen@gmail.com>
2021-03-05 02:14:47 +0800

    add bar

--- fryptor.py
+++ fryptor.py
@@ -1,27 +1,41 @@
 from hashlib import md5
 from argparse import ArgumentParser
+import logging
+from threading import Thread
 import os
+
+logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
+
+_total = 0
+_count = 0
 
 
 def _key_parser(key: str):
     seed = key.encode()
+    ptr = 0
     while True:
+        while ptr < len(seed):
+            yield seed[ptr]
+            ptr += 1
+        ptr = 0
         seed = md5(seed).digest()
-        yield seed[0]
 
 
 def _file_reader(path: str):
+    global _total
+    global _count
+    _total = os.path.getsize(path)
     with open(path, 'rb') as f:
-        for line in f:
-            for byte in line:
-                yield byte
+        while _count < _total:
+            yield ord(f.read(1))
+            _count += 1
 
 
 def _file_writer(path: str):
     with open(path, 'wb') as f:
         while True:
-            int_ = yield
-            f.write(bytes([int_]))
+            data = yield
+            f.write(bytes([data]))
 
 
 def process(src: str, dst: str, key: str):
@@ -29,14 +43,12 @@ def process(src: str, dst: str, key: str):
     if dst is None:
         dst = f'{src}.temp'
         replace = True
-        print('Output path not specified, source file will be replaced.')
 
     fr = _file_reader(src)
     fw = _file_writer(dst)
     pp = _key_parser(key)
 
     fw.send(None)
-    print('Processing')
     for i, j in zip(fr, pp):
         fw.send(i ^ j)
     fw.close()
@@ -45,16 +57,40 @@ def process(src: str, dst: str, key: str):
         os.remove(src)
         os.rename(dst, src)
 
-    print('Done')
-
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-i', '--in', help='path to source file', metavar='PATH', required=True, dest='src')
     parser.add_argument('-k', '--key', help='key used in encryption/decryption', required=True)
-    parser.add_argument('-o', '--out', help='path to output file',
-                        metavar='PATH', dest='dst')
+    parser.add_argument('-o', '--out', help='path to output file', metavar='PATH', dest='dst')
     args = parser.parse_args()
     if not os.path.exists(args.src):
         raise FileExistsError('Path to source file is wrong!')
-    process(args.src, args.dst, args.key)
+    if args.dst is None:
+        logging.warning('Output path not specified, source file will be replaced.')
+
+
+    def bar():
+        global _total
+        global _count
+        while _total == 0:
+            pass
+        while _count < _total:
+            print(f'{100 * _count / _total: .2f} %', end='\r')
+        print(f'{100 * _count / _total: .2f} %')
+
+
+    threads = [
+        Thread(target=process, args=(args.src, args.dst, args.key)),
+        Thread(target=bar),
+    ]
+
+    logging.info('Started processing')
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    logging.info('Done')
